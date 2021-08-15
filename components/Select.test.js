ce1c143f3856ce59cbb45f3e130ad09ba282d8c1
Josh Goldberg <josh@codecademy.com>
2021-08-15 00:11:23 -0400

    A few pre workshop touchups

--- components/Select.test.js
+++ components/Select.test.js
@@ -7,14 +7,15 @@ const renderView = setupRtl(Select);
 
 describe(Select, () => {
   it("fires onChange with the new value when an option is selected", () => {
-    const { props, view } = renderView({
-      onChange: jest.fn(),
+    const onChange = jest.fn();
+    const { view } = renderView({
+      onChange,
       options: ["apple", "banana", "cherry"],
       selected: "cherry",
     });
 
     userEvent.selectOptions(view.getByRole("combobox"), "banana");
 
-    expect(props.onChange).toHaveBeenCalledWith("banana");
+    expect(onChange).toHaveBeenCalledWith("banana");
   });
 });
