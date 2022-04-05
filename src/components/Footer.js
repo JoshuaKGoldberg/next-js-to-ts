0a4deb8af46379f911a37a2c04ad26ca9844b212
mehmet sayin <75525090+sayinmehmet47@users.noreply.github.com>
2022-04-05 23:38:47 +0200

    Update src/components/Footer.js
    Co-authored-by: Josh Goldberg <me@joshuakgoldberg.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import styles from "./Footer.module.css";
 
-export default function Footer() {
+export function Footer() {
   return (
     <footer className={styles.footer}>
       <a
